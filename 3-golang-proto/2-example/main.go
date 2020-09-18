280b139faf98abc2ed048732fad41baa5c8b0e14
Emad <emadghaffariii@gmail.com>
2020-09-18 18:05:53 +0000

    add go_option example

---
+++ 3-golang-proto/2-example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	PersonDB "github.com/Emadghaffari/proto-golang/4-golang-proto/2-example/tutorial"
+)
+
+func main() {
+	p1 := PersonDB.Person{
+		Id:          1,
+		FirstName:   "Emad",
+		IsValidated: true,
+		SomeArray:   []string{"Tag1", "Tag2"},
+		Color:       PersonDB.Person_RED,
+		Date:        &PersonDB.Person_MyDate{Year: 1996, Month: 12, Day: 26},
+	}
+	fmt.Println(p1.GetId())
+	fmt.Println(p1.GetFirstName())
+}
